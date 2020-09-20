31938c878a8bc07161aaf8167c4cb39d97b25003
郭暐勝 <ryankuo0326@gmail.com>
2020-09-20 22:20:57 +0800

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 
 namespace core.Controllers
 {
+    // UNBOXING 
+    // BONING
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -32,20 +34,6 @@ namespace core.Controllers
             data = new personInformation() { 
             };
         }
-
-        //[HttpGet]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    var rng = new Random();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = rng.Next(-20, 55),
-        //        Summary = Summaries[rng.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-
-        //}
 
         [HttpGet]
         public IEnumerable<personInformation> Get()
