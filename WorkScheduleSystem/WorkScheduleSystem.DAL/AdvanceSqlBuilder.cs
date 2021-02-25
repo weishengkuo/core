3dbca6565fde54aaf6a03d6d562935517e8b13a6
WeishengKuo <ryankuo0326@gmail.com>
2021-02-25 21:34:21 +0800

    排班人員

---
+++ WorkScheduleSystem/WorkScheduleSystem.DAL/AdvanceSqlBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using WorkScheduleSystem.BaseModels.Models;
+using WorkScheduleSystem.Common.AttributeExtend.Helper;
+
+namespace WorkScheduleSystem.DAL
+{
+    public class AdvanceSqlBuilder<T> where T : BaseModel
+    {
+        public static string FindSql = "";
+        public static string FindSqlAll = ""; 
+        public static string Add = "";
+        public static string Update = "";
+        public static string Delete = "";
+
+        private static Type type = typeof(T);
+        private static string TableName = AttributeHelper.GetTableMappingName<T>(); 
+
+        static AdvanceSqlBuilder()
+        {
+            Type type = typeof(T);       
+            string baseColumn = string.Join(",", type.GetProperties().Select(p => $"[{ p.GetColumnMappingName()}]"));                      
+            string baseColumnNoId = string.Join(",", type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).Select(p => $"[{p.GetColumnMappingName()}]"));
+
+
+            #region Select one
+            FindSql = $"SELECT {baseColumn} FROM [{ TableName }] WHERE Id = @Id";
+            #endregion
+
+            #region Select All
+            FindSqlAll = $"SELECT {baseColumn} FROM [{TableName}]";
+            #endregion
+
+
+            #region Add return New item
+            string addValueString = string.Join(",", type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .Select(p => $"@{p.Name}"));         
+            string addReturnValueString = string.Join(",", type.GetProperties()
+                .Select(p => $"INSERTED.[{p.GetColumnMappingName()}]"));  
+            Add = $@"INSERT [{TableName}] ({baseColumnNoId}) 
+                     VALUES ({addValueString});
+                     SELECT CAST(scope_identity() AS int);";
+            #endregion
+
+            #region Delete
+            Delete = $@"DELETE FROM [{TableName}] WHERE Id = @Id";
+            #endregion
+
+            #region Update
+            string updateColumnString = string.Join(",", type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).Select(p => $"[{p.Name}] = @{p.Name}"));
+            Update = $@"UPDATE [{TableName}] SET {updateColumnString} WHERE Id = @Id";
+            #endregion
+
+        }
+    }
+}
---
+++ WorkScheduleSystem/WorkScheduleSystem.DAL/BaseDAL.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using WorkScheduleSystem.BaseModels.Models;
+using WorkScheduleSystem.IDAL;
+using System.Data.SqlClient;
+using System.Linq;
+using WorkScheduleSystem.Common;
+using WorkScheduleSystem.Common.AttributeExtend.Helper;
+
+namespace WorkScheduleSystem.DAL
+{
+    public class BaseDAL : IBaseDAL
+    {        
+        public int Add<T>(T t) where T : BaseModel 
+        {           
+            Type type = t.GetType();
+            return ExecuteSql<int>(AdvanceSqlBuilder<T>.Add, (cmd) =>
+            {
+                var parameterList = type
+                               .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                               .Select(p => new SqlParameter($"@{p.Name}", p.GetValue(t) ?? DBNull.Value));
+                cmd.Parameters.AddRange(parameterList.ToArray());
+                return (int)cmd.ExecuteScalar();
+            });
+        }
+
+        public bool Delete<T>(T t) where T : BaseModel
+        {                      
+            return ExecuteSql<bool>(AdvanceSqlBuilder<T>.Delete, (cmd) => {
+                cmd.Parameters.AddWithValue("@Id", t.Id);
+                return cmd.ExecuteNonQuery() == 1;
+            });
+        }
+
+        public T ExecuteSql<T>(string sql, Func<SqlCommand, T> func)
+        {
+            using (SqlConnection conn = new SqlConnection(Singleton.GetConnectionString())) 
+            {
+                T result = default(T); 
+                string ErrorMsg = "";
+                SqlTransaction tran = null; 
+                try
+                {
+                    conn.Open(); 
+                    using (tran = conn.BeginTransaction())
+                    {
+                        using (SqlCommand cmd = new SqlCommand(sql, conn, tran)) 
+                        {
+                            //LogMethod.WriteLog($"[T-SQL] {sql}");
+                            result = func.Invoke(cmd); 
+                            tran.Commit(); 
+                        }
+                    }
+                }
+                catch (Exception ex1)
+                {
+                    try
+                    {
+                        ErrorMsg = $"{ex1}";
+                        tran.Rollback(); 
+                    }
+                    catch (Exception ex2)
+                    {
+                       
+                        ErrorMsg += $"\r\n{ex2}";
+                    }
+                }
+                finally
+                {
+                    // 若是有錯誤資料就寫log
+                    //if (!string.IsNullOrEmpty(ErrorMsg)) LogMethod.WriteLog(ErrorMsg, true);
+                }
+                return result;
+            }
+        }
+
+        private T Trans<T>(Type type, SqlDataReader dr) where T : BaseModel
+        {
+            object oObject = Activator.CreateInstance(type);
+
+            foreach (var prop in type.GetProperties())
+            {                         
+                prop.SetValue(oObject, dr[prop.GetColumnMappingName()] is DBNull ? null : dr[prop.GetColumnMappingName()]);
+            }
+
+            return (T)oObject;
+        }
+
+        public T Find<T>(int Id) where T : BaseModel
+        {           
+            Type type = typeof(T);
+            string sql = AdvanceSqlBuilder<T>.FindSql;
+            return ExecuteSql<T>(sql, (cmd) => {
+                cmd.Parameters.AddWithValue("@Id", Id);
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    if (dr.Read())
+                    {
+                        return Trans<T>(type, dr);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            });
+        }
+
+
+        public List<T> FindAll<T>() where T : BaseModel
+        {           
+            Type type = typeof(T);
+
+            string sql = AdvanceSqlBuilder<T>.FindSqlAll;
+
+            return ExecuteSql<List<T>>(sql, (cmd) =>
+            {
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    List<T> tList = new List<T>();
+                    while (dr.Read())
+                    {
+                        tList.Add(Trans<T>(type, dr));
+                    }
+                    return tList;
+                }
+            });
+        }
+        public bool Update<T>(T t) where T : BaseModel
+        {
+            
+            Type type = typeof(T);
+            string sql = AdvanceSqlBuilder<T>.Update;
+            var parameterList = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                    .Select(p => new SqlParameter(p.Name, p.GetValue(t) ?? DBNull.Value));
+            return ExecuteSql<bool>(sql, (cmd) =>
+            {
+                cmd.Parameters.AddRange(parameterList.ToArray());
+                cmd.Parameters.AddWithValue("@Id", t.Id);
+                return cmd.ExecuteNonQuery() == 1;
+            });
+
+        }
+    }
+}
