d061c0169390ca0a958a7cdef71057d9ad7e6a9e
WeishengKuo <ryankuo0326@gmail.com>
2021-02-25 21:30:25 +0800

    實作新增修改查詢刪除方法

---
+++ WorkScheduleSystem/WorkScheduleSystem.IDAL/IBaseDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using WorkScheduleSystem.BaseModels.Models;
+
+namespace WorkScheduleSystem.IDAL
+{
+    public interface IBaseDAL
+    {
+        T Find<T>(int Id) where T : BaseModel;
+        List<T> FindAll<T>() where T : BaseModel;
+        int Add<T>(T t) where T : BaseModel;
+        bool Update<T>(T t) where T : BaseModel;
+        bool Delete<T>(T t) where T : BaseModel; 
+    }
+}
