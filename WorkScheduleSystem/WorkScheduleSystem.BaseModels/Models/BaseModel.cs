889396f2a65331cb425446a5ff4db0779a8ae300
WeishengKuo <ryankuo0326@gmail.com>
2021-02-26 12:20:20 +0800

    資料比對PrimaryKey

---
+++ WorkScheduleSystem/WorkScheduleSystem.BaseModels/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkScheduleSystem.BaseModels.Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; } 
+    }
+}
