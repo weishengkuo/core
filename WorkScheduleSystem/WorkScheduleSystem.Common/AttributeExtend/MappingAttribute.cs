f30cdf92dbc64afbe06630a0e29f86e86eed9928
WeishengKuo <ryankuo0326@gmail.com>
2021-02-26 12:14:20 +0800

    資料表比對

---
+++ WorkScheduleSystem/WorkScheduleSystem.Common/AttributeExtend/MappingAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkScheduleSystem.Common.AttributeExtend
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+    public class MappingAttribute:Attribute
+    {
+        public string MappingName { get; set; }
+        public MappingAttribute(string mappingName)
+        {
+            this.MappingName = mappingName;
+        }
+    }
+}
